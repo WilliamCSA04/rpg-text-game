6bb141f9d19ee12d3438d168c66255c5d916796a
williamcsa04 <williamcaetano14@gmail.com>
2022-06-27 20:20:18 -0300

    :recycle: Character is now impl

--- src/game.rs
+++ src/game.rs
@@ -13,29 +13,7 @@ pub struct Character {
     healthy: HEALTHY,
 }
 
-pub fn build_character(
-    hp: u64,
-    mana: u64,
-    ad: u64,
-    ap: u64,
-    pr: u64,
-    mr: u64,
-    name: ColoredString,
-    class: CLASSES,
-    healthy: HEALTHY,
-) -> Character {
-    Character {
-        hp,
-        mana,
-        ad,
-        ap,
-        pr,
-        mr,
-        name,
-        class,
-        healthy,
-    }
-}
+impl Character {}
 
 pub enum CLASSES {
     MAGE,
@@ -50,60 +28,60 @@ pub enum HEALTHY {
 }
 
 pub fn build_offtank() -> Character {
-    build_character(
-        500,
-        500,
-        10,
-        20,
-        50,
-        50,
-        "Misthy".to_string().purple(),
-        CLASSES::OFFTANK,
-        HEALTHY::ALIVE,
-    )
+    Character {
+        hp: 500,
+        mana: 500,
+        ad: 10,
+        ap: 20,
+        pr: 50,
+        mr: 50,
+        name: "Misthy".to_string().purple(),
+        class: CLASSES::OFFTANK,
+        healthy: HEALTHY::ALIVE,
+    }
 }
 
 pub fn build_archer() -> Character {
-    build_character(
-        200,
-        100,
-        100,
-        10,
-        10,
-        5,
-        "Myumii".to_string().red(),
-        CLASSES::ARCHER,
-        HEALTHY::ALIVE,
-    )
+    Character {
+        hp: 200,
+        mana: 100,
+        ad: 100,
+        ap: 10,
+        pr: 10,
+        mr: 5,
+        name: "Myumii".to_string().red(),
+        class: CLASSES::ARCHER,
+        healthy: HEALTHY::ALIVE,
+    }
 }
 
 pub fn build_mage() -> Character {
-    build_character(
-        250,
-        1000,
-        5,
-        100,
-        5,
-        15,
-        "Liwphael".to_string().yellow(),
-        CLASSES::MAGE,
-        HEALTHY::ALIVE,
-    )
+    Character {
+        hp: 250,
+        mana: 1000,
+        ad: 5,
+        ap: 100,
+        pr: 5,
+        mr: 15,
+        name: "Liwphael".to_string().yellow(),
+        class: CLASSES::MAGE,
+        healthy: HEALTHY::ALIVE,
+    }
 }
 
 pub fn build_enemy() -> Character {
     let mut rng = rand::thread_rng();
-    build_character(
-        rng.gen_range(100..1000),
-        rng.gen_range(100..1000),
-        rng.gen_range(25..150),
-        rng.gen_range(50..200),
-        rng.gen_range(10..110),
-        rng.gen_range(15..130),
-        "Random".to_string().yellow(),
-        CLASSES::UNKNOWN,
-        HEALTHY::ALIVE,
-    )
+    Character {
+        hp: rng.gen_range(100..1000),
+        mana: rng.gen_range(100..1000),
+        ad: rng.gen_range(25..150),
+        ap: rng.gen_range(50..200),
+        pr: rng.gen_range(10..110),
+        mr: rng.gen_range(15..130),
+        name: "Random".to_string().yellow(),
+        class: CLASSES::UNKNOWN,
+        healthy: HEALTHY::ALIVE,
+    }
 }
 
 pub fn start() {
@@ -119,4 +97,18 @@ pub fn start() {
         "Enemy status = hp:{}, mana:{}, ad:{}, ap:{}, pr:{}, mr{}",
         enemy.hp, enemy.mana, enemy.ad, enemy.ap, enemy.pr, enemy.mr
     );
+    for i in 0..5 {
+        if is_dead(&myu) && is_dead(&misthy) && is_dead(&liw) {
+            println!("Game Over");
+            break;
+        }
+        if is_dead(&enemy) {
+            println!("You won!");
+            break;
+        }
+    }
+}
+
+fn is_dead(char: &Character) -> bool {
+    matches!(char.healthy, HEALTHY::DEAD)
 }
